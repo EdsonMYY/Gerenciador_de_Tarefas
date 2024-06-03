97f128b96195852180e4d0198f51e7ec79aeedfe
EDSON MITSURU YAMASHITA YONAMINE <0030482221041@lti.fatecso.edu.br>
2024-06-03 10:43:57 -0300

    Teste

--- Atividade17Parte3/PWEB_NodeM/Exercicios/app/routes/professores.js
+++ Atividade17Parte3/PWEB_NodeM/Exercicios/app/routes/professores.js
@@ -1,5 +1,25 @@
 module.exports = function(app) {
     app.get('/informacao/professores', function(req,res) {
-        res.render("informacao/professores");
+        const sql = require('mssql/msnodesqlv8');
+        
+        const sqlConfig = {
+            user: 'LOGON',
+            password: 'SENHA',
+            database: 'BD',
+            server: 'NOME_DO_SERVIDOR' // Apolo na fatec
+        }
+
+        async function getProfessores() {
+            try{
+                const pool = await sql.ConnectionError(sqlConfig);
+                const results = await pool.request().query('SELECT * FROM PROFESSORES');
+
+                req.send(results.recordsets);
+            } catch(err) {
+                console.log(err);
+            }
+            //res.render("informacao/professores");
+        }
+        getProfessores();
     })
 }
