69864b2a3e66b81d1ae1841608f200ce5bde03c2
EDSON MITSURU YAMASHITA YONAMINE <0030482221041@lti.fatecso.edu.br>
2024-06-10 12:20:57 -0300

    Atualizações

--- Atividade17Parte4/PWEB_NodeM/Exercicios/app/models/professormodel.js
+++ Atividade17Parte4/PWEB_NodeM/Exercicios/app/models/professormodel.js
@@ -1,13 +1,17 @@
-module.exports = function(){
- 
-    this.getProfessores = function(connection, callback){
-         connection.query('select * from professores', callback);
+module.exports = function () {
+
+    this.getProfessores = function (connection, callback) {
+        connection.query('select * from professores', callback);
     }
-   
-    this.getProfessor = function(connection, callback){
-         connection.query('select * from professores WHERE id_professor=1', callback);
+
+    this.getProfessor = function (connection, callback) {
+        connection.query('select * from professores WHERE id_professor=1', callback);
     }
-   
-     return this;
-   
-  }
+
+    this.salvarProfessor = function (professor, connection, callback) {
+        connection.query("INSERT INTO professores (NOME_PROFESSOR,EMAIL_PROFESSOR) VALUES ('" + professor.nome_professor + "','" + professor.email_professor + "')", callback);
+    }
+
+    return this;
+
+}
